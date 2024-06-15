7c245aaf5384fd0c6948509d68b2b11005c0c7e0
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2024-06-15 23:35:17 +0600

    deployment error solved

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Routers from "./routers/routers";
+import Routers from "./routers/Routers";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
